070559ec1476f73b0bc64e55b4f54772e4ba0c5b
uwubuho <samueldgposso1@gmail.com>
2020-12-16 18:05:19 -0500

    Created ping view

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""
+Views for chat app
+"""
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class PingPongView(APIView):
+    """
+    Pongs to Pings
+    """
+    def get(self, request):
+        return Response({ 'result' : 'Pong!' }, status=status.HTTP_200_OK)
