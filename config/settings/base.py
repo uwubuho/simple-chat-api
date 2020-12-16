52a892689ee183c753803f2313fa8c9fdfd22ccc
uwubuho <samueldgposso1@gmail.com>
2020-12-16 17:05:29 -0500

    Installed Rest Framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,8 +20,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'channels'
 ]
+
+REST_FRAMEWORK = {
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
