a23ecd28883f65b218de0c0e72d7997630647290
uwubuho <samueldgposso1@gmail.com>
2020-12-21 17:51:35 -0500

    Configured media files in local

--- config/settings/local.py
+++ config/settings/local.py
@@ -29,8 +29,11 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
