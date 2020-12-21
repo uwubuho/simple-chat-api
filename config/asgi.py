c4ca1ba38371e509af3cdafbf1accf798cb27c15
uwubuho <samueldgposso1@gmail.com>
2020-12-21 12:38:33 -0500

    Configured asgi application to run in production

--- config/asgi.py
+++ config/asgi.py
@@ -3,16 +3,18 @@ ASGI config for chat project.
 """
 
 import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import apps.chat.routing
+from apps.chat import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(apps.chat.routing.websocket_urlpatterns)
+        URLRouter(routing.websocket_urlpatterns)
     ),
 })
