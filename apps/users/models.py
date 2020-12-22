7b320c78ea8a047acd967ca5c5afa7e23aaa1eb3
uwubuho <samueldgposso1@gmail.com>
2020-12-22 00:23:23 -0500

    Created Friendship model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,7 +3,10 @@ Models for users app
 """
 
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
 from django.db.models.base import Model
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
@@ -11,9 +14,31 @@ from django.dispatch.dispatcher import receiver
 
 class UserProfile(Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True, related_name='profile')
-    nickname = models.CharField(max_length=150, null=False, default="Joe")
+    nickname = models.CharField(max_length=150, null=False, default='Joe')
     description = models.CharField(max_length=255, blank=True, null=False)
     picture = models.ImageField(upload_to='profiles', default='default-user.png')
+
+    @property
+    def friends(self):
+        return UserProfile.objects.filter(Q(buddies__fellow=self) | Q(fellows__buddy=self))
+
+
+class Friendship(Model):
+    fellow = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='fellows')
+    buddy = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='buddies')
+
+    def clean(self):
+        condition = Q(fellow=self.buddy, buddy=self.fellow) | Q(fellow=self.fellow, buddy=self.buddy)
+        exists = self.__class__.objects.filter(condition).exclude(id=self.id).exists()
+        if exists:
+            raise ValidationError(_("Friendship already exists beetween {} and {}".format(self.fellow, self.buddy)))
+        if self.fellow == self.buddy:
+            raise ValidationError(_("Can't declare friendship to yourself"))
+        return super().clean()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.full_clean()
+        return super().save(force_insert, force_update, using, update_fields)
 
 
 @receiver(post_save, sender=get_user_model())
--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -2,9 +2,11 @@
 Models Tests for users app
 """
 
+from django.core.exceptions import ValidationError
 from django.test.testcases import TestCase
 from django.contrib.auth import get_user_model
-from apps.users.models import UserProfile
+from django.db import transaction
+from apps.users.models import Friendship, UserProfile
 
 
 class ModelsTests(TestCase):
@@ -20,3 +22,34 @@ class ModelsTests(TestCase):
         self.assertEqual(profile.user.username, 'someuser')
         self.assertEqual(profile.user.email, 'example@user.com')
         self.assertEqual(profile.nickname, user.username)
+
+    def test_cant_create_duplicated_friendships(self):
+        """
+        Does not allow duplicated friendships
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        fellow = User.objects.create_user(username='fellow', email='fellow@user.com', password='1234').profile
+        buddy = User.objects.create_user(username='buddy', email='buddy@user.com', password='1234').profile
+        Friendship.objects.create(fellow=fellow, buddy=buddy)
+        with transaction.atomic():
+            self.assertRaises(ValidationError, Friendship.objects.create, fellow=fellow, buddy=buddy)
+        with transaction.atomic():
+            self.assertRaises(ValidationError, Friendship.objects.create, fellow=buddy, buddy=fellow)
+
+    def test_friends_property_works(self):
+        """
+        Friends property works
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        user = User.objects.create_user(username='joe', email='joe@user.com', password='1234').profile
+        friend1 = User.objects.create_user(username='friendjoe1', email='joe1@user.com', password='1234').profile
+        friend2 = User.objects.create_user(username='friendjoe2', email='joe2@user.com', password='1234').profile
+        lonelyone = User.objects.create_user(username='lonelyone', email='lonelyone@user.com', password='1234').profile
+        Friendship.objects.create(fellow=user, buddy=friend1)
+        Friendship.objects.create(fellow=friend2, buddy=user)
+        Friendship.objects.create(fellow=friend1, buddy=friend2)
+        self.assertTrue(user.friends.filter(pk=friend1).exists())
+        self.assertTrue(user.friends.filter(pk=friend2).exists())
+        self.assertFalse(user.friends.filter(pk=lonelyone).exists())
+        self.assertTrue(friend1.friends.filter(pk=friend2).exists())
+        self.assertTrue(friend1.friends.filter(pk=user).exists())
