52672dbe6ed8c1ef839cc166b2b8352c2e85d18b
uwubuho <samueldgposso1@gmail.com>
2020-12-22 16:21:32 -0500

    Created FriendRequest model

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, constraints
 from django.db.models.base import Model
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
@@ -36,9 +36,45 @@ class Friendship(Model):
             raise ValidationError(_("Can't declare friendship to yourself"))
         return super().clean()
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(self, *args, **kwargs):
         self.full_clean()
-        return super().save(force_insert, force_update, using, update_fields)
+        return super().save(*args, **kwargs)
+
+
+class FriendRequest(Model):
+    from_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sent_requests')
+    to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='received_requests')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        requests = self.__class__.objects.filter(to_user=self.from_user, from_user=self.to_user).exclude(id=self.id)
+        if requests.exists():
+            return requests[0].accept()
+        else:
+            return super().save(*args, **kwargs)
+
+    def clean(self):
+        condition = Q(fellow=self.from_user, buddy=self.to_user) | Q(fellow=self.to_user, buddy=self.from_user)
+        exists = Friendship.objects.filter(condition).exists()
+        if exists:
+            raise ValidationError(_("Friendship already exists beetween {} and {}".format(self.from_user, self.to_user)))
+        if self.from_user == self.to_user:
+            raise ValidationError(_("Can't declare friend request to yourself"))
+        return super().clean()
+
+    def accept(self):
+        friendship = Friendship.objects.create(fellow=self.from_user, buddy=self.to_user)
+        friendship.save()
+        self.delete()
+        return friendship
+
+    def reject(self):
+        self.delete()
+
+    class Meta:
+        constraints = [
+            constraints.UniqueConstraint(fields=['from_user', 'to_user'], name='unique_request')
+        ]
 
 
 @receiver(post_save, sender=get_user_model())
--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -5,8 +5,7 @@ Models Tests for users app
 from django.core.exceptions import ValidationError
 from django.test.testcases import TestCase
 from django.contrib.auth import get_user_model
-from django.db import transaction
-from apps.users.models import Friendship, UserProfile
+from apps.users.models import Friendship, FriendRequest, UserProfile
 
 
 class ModelsTests(TestCase):
@@ -31,10 +30,8 @@ class ModelsTests(TestCase):
         fellow = User.objects.create_user(username='fellow', email='fellow@user.com', password='1234').profile
         buddy = User.objects.create_user(username='buddy', email='buddy@user.com', password='1234').profile
         Friendship.objects.create(fellow=fellow, buddy=buddy)
-        with transaction.atomic():
-            self.assertRaises(ValidationError, Friendship.objects.create, fellow=fellow, buddy=buddy)
-        with transaction.atomic():
-            self.assertRaises(ValidationError, Friendship.objects.create, fellow=buddy, buddy=fellow)
+        self.assertRaises(ValidationError, Friendship.objects.create, fellow=fellow, buddy=buddy)
+        self.assertRaises(ValidationError, Friendship.objects.create, fellow=buddy, buddy=fellow)
 
     def test_friends_property_works(self):
         """
@@ -53,3 +50,69 @@ class ModelsTests(TestCase):
         self.assertFalse(user.friends.filter(pk=lonelyone).exists())
         self.assertTrue(friend1.friends.filter(pk=friend2).exists())
         self.assertTrue(friend1.friends.filter(pk=user).exists())
+
+    def test_request_accept(self):
+        """
+        Friendship created and request deleted when accepting it
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        ale = User.objects.create_user(username='ale', email='ale@user.com', password='1234').profile
+        mey = User.objects.create_user(username='mey', email='mey@user.com', password='1234').profile
+        request = FriendRequest.objects.create(from_user=ale, to_user=mey)
+        self.assertTrue(FriendRequest.objects.filter(pk=request.id).exists())
+        request.accept()
+        self.assertTrue(ale.friends.filter(pk=mey).exists())
+        self.assertTrue(mey.friends.filter(pk=ale).exists())
+        self.assertFalse(FriendRequest.objects.filter(pk=request.id).exists())
+
+    def test_request_reject(self):
+        """
+        Request deleted when rejecting it
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        crunchy = User.objects.create_user(username='crunchy', email='crunchy@user.com', password='1234').profile
+        buho = User.objects.create_user(username='buho', email='buho@user.com', password='1234').profile
+        request = FriendRequest.objects.create(from_user=buho, to_user=crunchy)
+        self.assertTrue(FriendRequest.objects.filter(pk=request.id).exists())
+        request.reject()
+        self.assertFalse(crunchy.friends.filter(pk=buho).exists())
+        self.assertFalse(buho.friends.filter(pk=crunchy).exists())
+        self.assertFalse(FriendRequest.objects.filter(pk=request.id).exists())
+
+    def test_both_way_request(self):
+        """
+        Request auto accept when two requests exist both ways
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        crunchy = User.objects.create_user(username='crunchy', email='crunchy@user.com', password='1234').profile
+        buho = User.objects.create_user(username='buho', email='buho@user.com', password='1234').profile
+        request_buho = FriendRequest.objects.create(from_user=buho, to_user=crunchy)
+        request_crunchy = FriendRequest.objects.create(from_user=crunchy, to_user=buho)
+        self.assertTrue(crunchy.friends.filter(pk=buho).exists())
+        self.assertTrue(buho.friends.filter(pk=crunchy).exists())
+        self.assertFalse(FriendRequest.objects.filter(pk=request_buho.id).exists())
+        self.assertFalse(FriendRequest.objects.filter(pk=request_crunchy.id).exists())
+
+    def test_cant_create_duplicated_request(self):
+        """
+        Can't create duplicated requests
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        fellow = User.objects.create_user(username='fellow', email='fellow@user.com', password='1234').profile
+        buddy = User.objects.create_user(username='buddy', email='buddy@user.com', password='1234').profile
+        FriendRequest.objects.create(from_user=fellow, to_user=buddy)
+        self.assertRaises(ValidationError, FriendRequest.objects.create, from_user=fellow, to_user=buddy)
+
+    def test_cant_create_requests_when_already_friend(self):
+        """
+        Can't create requests when both users are already friends
+        """
+        User = get_user_model() # pylint: disable=invalid-name
+        crunchy = User.objects.create_user(username='crunchy', email='crunchy@user.com', password='1234').profile
+        buho = User.objects.create_user(username='buho', email='buho@user.com', password='1234').profile
+        self.assertFalse(crunchy.friends.filter(pk=buho).exists())
+        request = FriendRequest.objects.create(from_user=buho, to_user=crunchy)
+        request.accept()
+        self.assertTrue(crunchy.friends.filter(pk=buho).exists())
+        self.assertRaises(ValidationError, FriendRequest.objects.create, from_user=buho, to_user=crunchy)
+        self.assertRaises(ValidationError, FriendRequest.objects.create, from_user=crunchy, to_user=buho)
